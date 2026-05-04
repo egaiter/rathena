ec4fc693552a8c07c63b9ddf35c74d8478cbbaff
Elijah Gaiter <elijahc280@gmail.com>
2026-05-04 00:37:17 -0600

    Tweak storage limits

--- src/common/mmo.hpp
+++ src/common/mmo.hpp
@@ -95,7 +95,7 @@ typedef uint32 t_itemid;
 #define MIN_WALK_SPEED 20 ///Min walk speed
 #define MAX_WALK_SPEED 1000 ///Max walk speed
 #define MAX_STORAGE 1200 ///Max number of storage slots a player can have
-#define MAX_GUILD_STORAGE 1200 ///Max number of storage slots a guild
+#define MAX_GUILD_STORAGE 600 ///Max number of storage slots a guild
 #define MAX_PARTY 12 ///Max party member
 #define MAX_GUILD 16+10*6	///Increased max guild members +6 per 1 extension levels [Lupus]
 #define MAX_GUILDPOSITION 20	///Increased max guild positions to accommodate for all members [Valaris] (removed) [PoW]
